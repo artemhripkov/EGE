919e1192fc397af7de315f60df8b8d2ad4d18016
artemhripkov <artemhripkov1985@mail.ru>
2025-01-16 10:44:01 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -18,6 +18,17 @@ def f(n):
     if n>2:
         return n*f(n-2)
 print(f(5000)/f(4994))
+#############################
+# 17 задание
+a = [int(x) for x in open('17-243.txt')]
+
+S = sum([int(i) for x in a if x % 49 == 0 for i in str(x)])
+ans = []
+for i in range(len(a) - 1):
+    if (a[i] > S and (not a[i + 1] > S) and a[i + 1] % 10 == 7) or \
+            (a[i + 1] > S and (not a[i] > S) and a[i] % 10 == 7):
+        ans.append(a[i] + a[i + 1])
+print(len(ans), min(ans))
 ############################
 # 19-21 задание
 def f(a,b,m):
@@ -41,18 +52,19 @@ def f(c,e):
         return f(c+1, e)+f(2*c, e)+f(2*c+1,e) 
 print(f(4,218))
 #############################
-# 17 задание
-a = [int(x) for x in open('17-243.txt')]
-
-S = sum([int(i) for x in a if x % 49 == 0 for i in str(x)])
-ans = []
-for i in range(len(a) - 1):
-    if (a[i] > S and (not a[i + 1] > S) and a[i + 1] % 10 == 7) or \
-            (a[i + 1] > S and (not a[i] > S) and a[i] % 10 == 7):
-        ans.append(a[i] + a[i + 1])
-print(len(ans), min(ans))
-
-
-
+# 25 задание
+def div(x):
+    d = set()
+    for i in range(1,int(x**0.5)+1):
+        if x%i==0:
+            d.add(i)
+            d.add(x//i)
+    return sorted(d)
+for x in range(244143, 367821+1):
+    #если число является квадратом целого числа 
+    if int(x**0.5)**2 == x:
+        d = div(x)
+        if len(d)==5:
+            print(d[3],d[4])
 
 
