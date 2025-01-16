e434340c183b09cf9c094c969309b9a70c801052
artemhripkov <artemhripkov1985@mail.ru>
2025-01-16 10:41:43 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,1 +1,46 @@
-print('жопа')
+# 2 задание
+from itertools import *
+def f(x,y,w,z):
+  return (x==(not y))<=((x and w)==z)
+for a1,a2,a3,a4,a5 in product([0,1],repeat=5):
+    table = [(1,1,a1,a2),(1,1,a3,1),(a4,1,1,a5)] 
+    if len(table)==len(set(table)):
+        for p in permutations('xywz'):
+            if [f(**dict(zip(p,r))) for r in table]==[0,0,0]: 
+                print(p)
+############################
+# 16 задание
+from sys import * 
+setrecursion limit(10000)
+def f(n):
+    if n<=2: 
+        return 1
+    if n>2:
+        return n*f(n-2)
+print(f(5000)/f(4994))
+############################
+# 19-21 задание
+def f(a,b,m):
+    if a+b>=231: return m%2==0
+    if m==0: return 0
+    h = [f(a+2,b,m-1),f(a*2,b,m-1),f(a,b+2,m-1),f(a,b*2,m-1)]
+    return any(h) if (m-1)%2==0 else all|(h)
+print('19)',[s for s in range(1,214) if f(17,s,2)]) #all->any
+print('20)',[s for s in range(1,214) if not f(17,s,1) and f(17,s,3)])
+print('21)',[s for s in range(1,214) if not f(17,s,2) and f(17,s,4)])
+############################
+# 23 задание
+from functools import *
+alru_cache(None)
+def f(c,e):
+    if c>e: \
+        return 0
+    if c==e:
+        return 1
+    if c<e:
+        return f(c+1, e)+f(2*c, e)+f(2*c+1,e) 
+print(f(4,218))
+
+
+
+
